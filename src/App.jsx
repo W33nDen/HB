379c3e74923e47abd12360a717cf7b41e93bdc44
W33nDen <email@example.com>
2026-05-16 21:24:28 +0200

    Fix image paths for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ import { Camera, Star, MessageCircle, Heart, Sparkles } from 'lucide-react';
 import confetti from 'canvas-confetti';
 
 const IMAGES = [
-  '/img/2911a4f4-3ecd-4e0f-8e99-c17614c30466.jpg',
-  '/img/5ea8cbba-755a-4c5a-936c-3c2f35733334.jpg',
-  '/img/d0a37efe-211a-4781-ab53-d9d14aba05ff.jpg',
-  '/img/photo_2026-01-01_23-41-11.jpg',
-  '/img/photo_2026-05-16_21-00-38.jpg',
-  '/img/photo_2026-05-16_21-03-32.jpg',
-  '/img/photo_2026-05-16_21-04-20.jpg'
+  './img/2911a4f4-3ecd-4e0f-8e99-c17614c30466.jpg',
+  './img/5ea8cbba-755a-4c5a-936c-3c2f35733334.jpg',
+  './img/d0a37efe-211a-4781-ab53-d9d14aba05ff.jpg',
+  './img/photo_2026-01-01_23-41-11.jpg',
+  './img/photo_2026-05-16_21-00-38.jpg',
+  './img/photo_2026-05-16_21-03-32.jpg',
+  './img/photo_2026-05-16_21-04-20.jpg'
 ];
 
 const VIDEOS = [
-  '/img/IMG_5539.MOV',
-  '/img/IMG_5176.MOV',
-  '/img/IMG_5532.MOV',
-  '/img/video.mp4'
+  './img/IMG_5539.MOV',
+  './img/IMG_5176.MOV',
+  './img/IMG_5532.MOV',
+  './img/video.mp4'
 ];
 
 const MEMORIES = [
